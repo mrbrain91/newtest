cd59dacca8039bfd94208ec630e6d81f88108086
MacBook Pro <f.yuldashev.tuit@gmail.com>
2023-04-29 21:08:40 +0500

    fixed spravochnik and prepayment list

--- counterparties.php
+++ counterparties.php
@@ -66,7 +66,7 @@ if (isset($_POST['id_contractor'])) {
 <html lang="en">
 <head>
   
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -147,7 +147,7 @@ if (isset($_POST['id_contractor'])) {
        $sts_color = "green";
     }else {
         $sts = "Не активный";
-       $sts_color = "black";
+        $sts_color = "black";
     }
 ?> 
             <tr data-toggle="collapse" data-target="#row<?php echo $i;?>" aria-expanded="true" class="accordion-toggle">
--- prepayment_list.php
+++ prepayment_list.php
@@ -8,8 +8,8 @@ if (!isset($_SESSION['usersname'])) {
 
 
 
-$query = "SELECT * FROM debts WHERE main_prepayment!='0' AND sts='2' ORDER BY id DESC";
-$rs_result = mysqli_query ($connect, $query);   
+// $query = "SELECT * FROM debts WHERE main_prepayment!='0' AND sts='2' ORDER BY id DESC";
+// $rs_result = mysqli_query ($connect, $query);   
 
 
 
@@ -66,6 +66,11 @@ $all_debt_fetch = mysqli_fetch_array($all_debt_result);
 $all_debt = $all_debt_fetch['all_debt'];
 $all_count = $all_debt_fetch['allcount'];
 
+
+if ($all_count < $limit) {
+    $limit  = $all_count;
+  }
+
 //for list 
 $rs_result = mysqli_query ($connect, $query);
 
@@ -78,13 +83,14 @@ $rs_result = mysqli_query ($connect, $query);
 
 
 <!DOCTYPE html>
-<html lang="ru">
+<html lang="en">
 <head>
   
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -95,7 +101,7 @@ $rs_result = mysqli_query ($connect, $query);
     <title>ortosavdo</title>
     
 </head>
-<body>  
+<body>   
 
 <?php include 'partSite/nav.php'; ?>
 
@@ -107,49 +113,52 @@ $rs_result = mysqli_query ($connect, $query);
     </div>    
 </div>
 
+
+
+<!-- Toolbar-->
 <div class="toolbar">
         <div class="container-fluid">
             <div class="toolbar_wrapper">
-                <div class="toolbar_wrapper">
-                    <div><a href="prepayment_add.php"> <button type="button" class="btn btn-success">Добавить</button> </a></div>
-                    <div class="filter-container">
-                        <div style="background-color:<?php echo $bg_sts;?>" class="filter-container-item first" data-toggle="modal" data-target="#filter">
-                        <span class="glyphicon glyphicon-filter"></span>
+                <div>
+                    <a href="prepayment_add.php"> <button type="button" class="btn btn-success">Добавить</button> </a>
+                </div>
+                <div class="filter-container">
+                    <div style="background-color:<?php echo $bg_sts;?>" class="filter-container-item first" data-toggle="modal" data-target="#filter">
+                    <span class="glyphicon glyphicon-filter"></span>
+                    </div>
+                    <div style="display:<?php echo $display_sts;?>" class="filter-container-item">
+                        <span><span id="row_c"><?php echo $limit; ?></span> / <?php echo $all_count; ?></span>
+                    </div>
+                    <div style="display:<?php echo $display_sts_filer_on;?>"class="filter-container-item">
+                        <span><?php echo $all_count?> / <?php echo $all_count; ?></span>
+                    </div>
+                    <div style="display:<?php echo $display_sts;?>" class="filter-container-item">
+                        <div class="loadmore">
+                            
+                            <button class="btn btn-outline-info" type="button" id="loadBtn" value="+10"><span class="glyphicon glyphicon-arrow-down"></span></button>
+                            <button style="display:none;" class="btn btn-outline-info" type="button" id="endBtn" value="+10"><span class="glyphicon glyphicon-ok"></span></button>
+                            <input type="hidden" id="row" value="0">
+                            <input type="hidden" id="postCount" value="<?php echo $postCount; ?>">
                         </div>
-                        <div style="display:<?php echo $display_sts;?>" class="filter-container-item">
-                            <span><span id="row_c"><?php echo $limit; ?></span> / <?php echo $all_count; ?></span>
-                        </div>
-                        <div style="display:<?php echo $display_sts_filer_on;?>"class="filter-container-item">
-                            <span><?php echo $all_count?> / <?php echo $all_count; ?></span>
-                        </div>
-                        <div style="display:<?php echo $display_sts;?>" class="filter-container-item">
-                            <div class="loadmore">
-                                
-                                <button class="btn btn-outline-info" type="button" id="loadBtn" value="+10"><span class="glyphicon glyphicon-arrow-down"></span></button>
-                                <button style="display:none;" class="btn btn-outline-info" type="button" id="endBtn" value="+10"><span class="glyphicon glyphicon-ok"></span></button>
-                                <input type="hidden" id="row" value="0">
-                                <input type="hidden" id="postCount" value="<?php echo $postCount; ?>">
-                            </div>
-                        </div>
-                    </div>  
+                    </div>
+                </div>  
             </div>
+
         </div>
 </div>
+<!--  -->
 
+<!-- Prepayment list -->
 <div class="all_table" style="margin-top:5px;">
     <div class="container-fluid">
-        <table class="table table-hover">
+        <table class="table table-hover" style="border-collapse:collapse;">
         <thead>
             <tr>
-            <th scope="col">Контрагент</th>
-            <!-- <th scope="col">Торговый представитель</th> -->
-            <th scope="col">Дата оплата</th>
-            <th scope="col">Тип оплаты</th>
-            <th scope="col">Сумма оплата</th>
-            <th scope="col">Просмотр</th>
-            <th scope="col">Редактировать</th>
-            
-
+                <th scope="col">Контрагент</th>
+                <!-- <th scope="col">Торговый представитель</th> -->
+                <th scope="col">Дата оплата</th>
+                <th scope="col">Тип оплаты</th>
+                <th scope="col">Сумма оплата</th>
             </tr>
         </thead>
         <tbody class="postList">
@@ -157,17 +166,28 @@ $rs_result = mysqli_query ($connect, $query);
         <?php     
             $i = 0;
             while ($row = mysqli_fetch_array($rs_result)) {
-            $i++;
+                $i++;
+                if ($row['sts'] == 1) {
+                    $sts = "Активный";
+                    $sts_color = "green";
+                }else {
+                    $sts = "Не активный";
+                    $sts_color = "black";
+                }
         ?> 
+            <tr data-toggle="collapse" data-target="#row<?php echo $i;?>" aria-expanded="true" class="accordion-toggle">
+                <td><?php $user = get_contractor($connect, $row["id_counterpartie"]); echo $user["name"];?></td>
+                <td><?php echo $date = date("d.m.Y", strtotime($row["order_date"])); ?></td>
+
+                <td><?php echo $row['payment_type']; ?></td>
+                <td><?php echo number_format($row['main_prepayment'], 0, ',', ' '); ?></td>
+            </tr>
             <tr>
-            <td><?php $user = get_contractor($connect, $row["id_counterpartie"]); echo $user["name"];?></td>
-            <td><?php echo $date = date("d.m.Y", strtotime($row["order_date"])); ?></td>
-
-            <td><?php echo $row['payment_type']; ?></td>
-            <td><?php echo number_format($row['main_prepayment'], 0, ',', ' '); ?></td>
-            <td><a href="#">Просмотр</a></td>
-            <td><a href="#">Редактировать</a></td>
-
+                <td colspan="12" style="border:0px;  background-color: #fafafb;" class="hiddenRow"><div class="accordian-body collapse" id="row<?php echo $i;?>"> 
+                    <!-- <a href="counterpartie_view.php?id=<?php echo $row["id"]; ?>"><button class="btn btn-custom">Просмотр</button> </a> -->
+                    <a href="counterpartie_edit.php?id=<?php echo $row["id"]; ?>"><button class="btn btn-custom">Редактировать</button> </a>
+                    <a href="action.php?change_sts_counterpartie_id=<?=$row['id']?>"><button class="btn btn-custom" onclick="return confirm('Изменить?')">Изменить стутус</button> </a>
+                </td>   
             </tr>
         <?php       
             };     
@@ -182,15 +202,15 @@ $rs_result = mysqli_query ($connect, $query);
         </table>
     </div>
 </div>
+<!--  -->
 
-
-
-
+<!-- Modals -->
 <div class="container-fluid">
 
     <?php include 'partSite/modal.php'; ?>
     
 </div>
+<!--  -->
 
 <!-- Modal filter-->
 <div class="modal fade" id="filter" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -246,20 +266,30 @@ $rs_result = mysqli_query ($connect, $query);
         <button type="submit" class="btn btn-secondary" form="filer_cencel">Показать все</button>
         <button type="button" class="btn btn-secondary" data-dismiss="modal">закрыть</button>
       </div>
-    </div>
+    </div> 
   </div>
 </div>  
 
-<!-- END MODAL -->
+<!-- -->
 
-
+<!-- Connect outsite js scripts -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.15.2/js/selectize.min.js"></script>
+
 <script>
+     $('.accordian-body').on('show.bs.collapse', function () {
+    $(this).closest("table")
+        .find(".collapse.in")
+        .not(this)
+        // .collapse('toggle')
+})
+// set class from selectize library
+$('.normalize').selectize();
+// 
 
-// $('.normalize').selectize();
 
+// load btn script
 $(document).ready(function () {
     $(document).on('click', '#loadBtn', function () {
         
@@ -292,6 +322,8 @@ $(document).ready(function () {
       });
     });
   });
+// 
+
 </script>
 </body>
 </html>
