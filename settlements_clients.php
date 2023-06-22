51a8714bc0627b2a0494a158a4263e9d3151f5c2
MacBook Pro <f.yuldashev.tuit@gmail.com>
2023-06-22 10:59:44 +0500

    fixed vzaimaraschet

--- settlements_clients.php
+++ settlements_clients.php
@@ -46,7 +46,7 @@ $rs_result = mysqli_query ($connect, $query);
 
 <div class="toolbar">
         <div class="container-fluid">
-           <a href="booked_payment_list.php"> <button type="button" class="btn btn-primary">история взаимарасчетов</button> </a>
+           <!-- <a href="booked_payment_list.php"> <button type="button" class="btn btn-primary">история взаимарасчетов</button> </a> -->
         </div>
 </div>
 
@@ -55,74 +55,87 @@ $rs_result = mysqli_query ($connect, $query);
         <table class="table table-hover" style="margin-bottom:0; border-collapse:collapse;">
         <thead>
             <tr>
-                <th scope="col">№</th>
+                <th scope="col">ID</th>
                 <th scope="col">Контрагент</th>
                 <th scope="col">ИНН</th>
-                <th scope="col">Долг</th>
-                <th scope="col">Предоплата</th>
-                <th scope="col">Итог</th>
+                <!-- <th scope="col">Долг</th> -->
+                <!-- <th scope="col">Предоплата</th> -->
+                <th scope="col" style="text-align:right;">Сумма</th>
             </tr>
         </thead>
         <tbody>
+<?php
+    $i = 0;
+    $data = array(); // Array to store the row data
 
+    while ($row = mysqli_fetch_array($rs_result)) {
+        $last_debt = $row['total_debt'] - $row['total_prepayment'];
+        $last_tot = $row['total_main_prepayment'] - $row['total_prepayment'];
+        $sum = $last_tot - $last_debt;
 
-            <?php     
-                $i = 0;
-                $sum_last_debt = 0;
-                $sum_last_tot = 0;
-                $balance_sum = 0;
+        $sum_last_debt += $last_debt;
+        $sum_last_tot += $last_tot;
+        $balance_sum += $sum;
 
+        if ($last_debt == 0 && $last_tot == 0) {
+            $display = 'none';
+            $i++;
+        } else {
+            $display = 'true';
+            $i++;
+        }
 
-                
-                while ($row = mysqli_fetch_array($rs_result)) {
-                
-                
-                $last_debt = $row['total_debt']- $row['total_prepayment'];
-                $last_tot = $row['total_main_prepayment'] - $row['total_prepayment'];
-                $sum = $last_tot - $last_debt;
+        // Store row data in the array
+        $data[] = array(
+            'i' => $i,
+            'sum' => $sum,
+            'row' => $row
+        );
+    }
 
-                $sum_last_debt += $last_debt;
-                $sum_last_tot += $last_tot;
-                $balance_sum += $sum;
-                
-                
-                if ($last_debt == 0 AND $last_tot == 0 ) {
-                    $display = 'none';
-                    $i++;
-                  }else {
-                    $display = 'true';
-                    $i++;
-                  }
-            ?> 
+    // Sort the array based on the 'sum' value in ascending order
+    usort($data, function($a, $b) {
+        if ($a['sum'] < 0 && $b['sum'] >= 0) {
+            return -1; // Negative values first
+        } elseif ($a['sum'] >= 0 && $b['sum'] < 0) {
+            return 1; // Positive values after negative values
+        } else {
+            return $a['sum'] - $b['sum']; // Sort by sum in ascending order
+        }
+    });
 
-            <tr data-toggle="collapse" data-target="#row<?php echo $i;?>" aria-expanded="true" class="accordion-toggle">
-                <td><?php echo $i; ?></td>
-                <td  style="background-color:<?php echo $bg_clr;?>"><?php $user = get_contractor($connect, $row["id_counterpartie"]); echo $user["name"];?></td>
-                <td><?php echo $user['inn']?></td>
-                <td><?php echo number_format($last_debt, 0, ',', ' '); ?></td>
-                <td><?php echo number_format($last_tot, 0, ',', ' '); ?></td>
-                <td><?php echo number_format($sum, 0, ',', ' '); ?></td>
-            </tr>
-            <tr>
-                <td colspan="12" style="border:0px;  background-color: #fafafb;" class="hiddenRow"><div class="accordian-body collapse" id="row<?php echo $i;?>"> 
-                    <a href="settlement_debts_detail.php?id=<?php echo $row["id_counterpartie"]?>&&prepayment=<?php echo $last_tot;?>"><button class="btn btn-custom">Перерасчёт</button> </a>
-                </td>
-            </tr>
+    // Iterate through the sorted data to generate the table rows
+    foreach ($data as $item) {
+        $i = $item['i'];
+        $sum = $item['sum'];
+        $row = $item['row'];
+?>
 
-            <?php       
-                };     
-            ?>
-        </tbody>
-        </table>
-        <table class="table" style="background-color:#ebf0ff; border-left: 4px solid #7396ff;">
-            <tr>
-                <td style="text-align:center;">Количество: <?php echo number_format($i, 0, ',', ' '); ?></td>
-                <td style="text-align:center;">Сумма долгов: <?php echo number_format($sum_last_debt, 0, ',', ' '); ?></td>
-                <td style="text-align:center;">Сумма предоплат: <?php echo number_format($sum_last_tot, 0, ',', ' '); ?></td>
-                <td style="text-align:center;">Итоговая сумма: <?php echo number_format($balance_sum, 0, ',', ' '); ?></td>
-                
-            </tr>
-        </table>
+<tr data-toggle="collapse" data-target="#row<?php echo $i;?>" aria-expanded="true" class="accordion-toggle">
+    <td><?php echo $i; ?></td>
+    <td style="background-color:<?php echo $bg_clr;?>"><?php $user = get_contractor($connect, $row["id_counterpartie"]); echo $user["name"];?></td>
+    <td><?php echo $user['inn']?></td>
+    <td class="text-right"><?php echo number_format($sum, 0, ',', ' '); ?></td>
+</tr>
+        <tr>
+            <td colspan="12" style="border:0px; background-color: #fafafb;" class="hiddenRow"><div class="accordian-body collapse" id="row<?php echo $i;?>"> 
+                <!-- Additional content for the expanded row -->
+            </td>
+        </tr>
+
+<?php
+    }
+?>
+</tbody>
+<table class="table" style="background-color:#ebf0ff; border-left: 4px solid #7396ff;">
+    <tr>
+        <!-- <td style="text-align:center;">Количество: <?php echo number_format($i, 0, ',', ' '); ?></td> -->
+        <!-- <td style="text-align:center;">Сумма долгов: <?php echo number_format($sum_last_debt, 0, ',', ' '); ?></td> -->
+        <!-- <td style="text-align:center;">Сумма предоплат: <?php echo number_format($sum_last_tot, 0, ',', ' '); ?></td> -->
+        <td style="text-align:right;">Итоговая сумма: <?php echo number_format($balance_sum, 0, ',', ' '); ?></td>
+    </tr>
+</table>
+
     </div>
 </div>
 
